5fc8c471d468c11ce77c7eb6a2d29544c13d9ce1
Victor Rutskin <Lashaka250375@gmail.com>
2023-01-31 21:25:27 +0200

    alpha version finish routing

--- src/app/Components/Titles/contact-title/contact-title.component.ts
+++ src/app/Components/Titles/contact-title/contact-title.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-title.component.css'],
 })
 export class ContactTitleComponent {
-  Title = 'Contact Us!';
+  Title = 'Contact Me!';
 }
