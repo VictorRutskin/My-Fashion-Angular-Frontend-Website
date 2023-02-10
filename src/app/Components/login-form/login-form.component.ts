1a3a350295cccafbb7fef9bf0a165dbd7e2e72e0
Victor Rutskin <Lashaka250375@gmail.com>
2023-02-10 14:41:32 +0200

    login works, need to add auth now

--- src/app/Components/login-form/login-form.component.ts
+++ src/app/Components/login-form/login-form.component.ts
@@ -7,36 +7,57 @@ import {
   Validators,
   FormBuilder,
 } from '@angular/forms';
+
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css'],
 })
 export class LoginFormComponent {
-  form = new FormGroup({
-    username: new FormControl(null, Validators.required),
-    password: new FormControl(null, Validators.required),
-  });
+  registerForm: any = FormGroup;
+  submitted = false;
+  //Add user form actions
+  get form() {
+    return this.registerForm.controls;
+  }
 
   constructor(
-    private authenticationService: AuthenticationService,
-    private Router: Router
+    //private authenticationService: AuthenticationService,
+    private formBuilder: FormBuilder
   ) {}
 
   submitForm() {
-    const username = this.form.get('username')?.value || '';
-    const password = this.form.get('password')?.value || '';
+    // const email = this.form.get('email')?.value || '';
+    // const password = this.form.get('password')?.value || '';
 
-    console.log(username, password);
+    // console.log(email, password);
 
-    this.authenticationService
-      .login(username, password)
-      .subscribe((response: any) => {
-        this.Router.navigate(['/Shop']);
-      });
+    // this.authenticationService
+    //   .login(username, password)
+    //   .subscribe((response: any) => {
+    //     this.Router.navigate(['/Shop']);
+    //   });
 
-    if (this.form.invalid) {
+    this.submitted = true;
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
       return;
     }
+    //True if all the fields are filled
+    if (this.submitted) {
+      alert('submitted!!');
+    }
+  }
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/),
+        ],
+      ],
+      password: ['', [Validators.required]],
+    });
   }
 }
